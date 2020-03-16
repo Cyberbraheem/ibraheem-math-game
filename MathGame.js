11712c2977d68de8ee6931afef2d94b52203e90c
Cyberbraheem <53270094+Cyberbraheem@users.noreply.github.com>
2020-03-16 08:32:41 -0400

    Update MathGame.js

--- MathGame.js
+++ MathGame.js
@@ -14,16 +14,6 @@ Variable Dictionary:
     product: holds the product of num1 and num2
     possibilites: the array which holds the order that the possible answers come in
 
-
-Program: 
-    This game outputs a random single digit multiplication question and the 4 answers. 
-    Only 1 is correct and the one correct is moved around so that is's not always in the same box. 
-    There is 60 seconds before the time runs out and it's game over. 
-    Every question correct, you gain a point and evey question wrong, you lose a point unless the score is 0. 
-    Then you don't lose any points so as not to go into negative numbers.
-    There is a reset button which allows you to go back to the start page. 
-    When the timer runs out, there is a game over screen and a play again button.
-
 */
 import React, { useState, useEffect } from 'react'
 function MathGame() {
