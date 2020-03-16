dfd0668bbd929bfcd7b08c3f59edb3e19f9728c3
Cyberbraheem <53270094+Cyberbraheem@users.noreply.github.com>
2020-03-16 08:30:05 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,4 @@
-// import React from "react"
-// import Header from "./Header"
-// import MemeGenerator from "./MemeGenerator"
-
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       <MemeGenerator />
-//     </div>
-//   )
-// }
-
-// export default App
 import React from "react"
-import MemeGenerator from "./MemeGenerator"
-import Calculator from './Calculator'
 import MathGame from './MathGame'
 
 function App() {
